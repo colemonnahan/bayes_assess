bed3ab4b645e005220e31df5a04458059478e740
colemonnahan <monnahc@uw.edu>
2018-04-24 11:08:26 -0700

    Clean up comparison files a bit

--- run_comparison_chains.R
+++ run_comparison_chains.R
@@ -5,53 +5,34 @@ library(adnuts)
 library(snowfall)
 recompile <- TRUE # recomile .tpl files?
 
+## I'm using a smaller warmup since we're not using mass matrix adaptation,
+## and starting from the mode so the chains need less warmup time.
+
+## adapt_delta (ad) and max_treedepth (td) are set at defaults unless
+## needed to be tweaked. inits NULL means to use the mode.
+
 reps <- 5                        # chains/reps to run
 iter <- 2000; warmup <- (iter/10)
-m <- d <- 'halibut3'
-ad <- .8                                # adapt_delta
-td <- 12
+m <- 'halibut3'
+ad <- .8; td <- 12
 inits <- NULL
 source('template.R')
 
 reps <- 5                        # chains/reps to run
 iter <- 2000; warmup <- (iter/10)
-m <- d <- 'hake'
-ad <- .8                                # adapt_delta
-td <- 12
+m <- 'hake'
+ad <- .8; td <- 12
 inits <- NULL
 source('template.R')
 
 reps <- 5                        # chains/reps to run
-iter <- 500; warmup <- (iter/10)
-m <- d <- 'canary2'
-ad <- .8                                # adapt_delta
-td <- 10
+iter <- 2000; warmup <- (iter/10)
+m <- 'canary2'
+ad <- .8; td <- 12
 source('template.R')
 
-reps <- 3                        # chains/reps to run
-iter <- 1000; warmup <- (iter/10)
-m <- d <- 'tanner2'
-ad <- .8                                # adapt_delta
-td <- 10
+reps <- 5                        # chains/reps to run
+iter <- 2000; warmup <- (iter/10)
+m <- 'tanner2'
+ad <- .8; td <- 10
 source('template.R')
-
-## Old code to look at unbounded space
-rotated <- read.csv(file.path(d,'rotated.csv'), head=FALSE)
-unbounded <- read.csv(file.path(d,'unbounded.csv'), head=FALSE)
-## Examine pairs in bounded space.
-posterior <- extract_samples(fit.rwm.mle, inc_lp=TRUE)
-ess <- fit.rwm.mle$ess
-pars <- names(sort(ess))[1:8]
-pairs_admb(posterior=posterior, mle=fit.rwm.mle$mle, pars=pars)
-launch_shinyadmb(fit.rwm.mle)
-## Now convert to unbounded and check
-mle <- fit.rwm.mle$mle
-unbounded$lp__ <- posterior$lp__
-names(unbounded) <- names(posterior)
-mle$cov <- fit.rwm.mle$covar.est
-mle$se <- sqrt(diag(mle$cov))
-mle$cor <- cov2cor(mle$cov)
-mle$est <- apply(unbounded, 2, mean)
-pairs_admb(posterior=unbounded, mle=mle, pars=pars, diag='trace')
-
-
